c15fd4308def69d383674c5010d2236501c34b23
r-dug <80994012+r-dug@users.noreply.github.com>
2021-05-26 13:05:41 -0700

    Create SMA_Plot.py

---
+++ SMA_Plot.py
@@ -0,0 +1,8 @@
+'''this file takes in an asset (ex. the array from the data scraping tool), and a timespan (integer value representing days) 
+and returns the plotted moving average of the asset, for the given timespan'''
+import matplotlib.pyplot as plt
+def plotMA_Close(asset, timespan)
+  asset['Open'].plot(figsize=(16,8))
+  asset['MA'] = asset['Open'].rolling(timespan).mean()
+  asset['MA'].plot(label=f'MA{timespan}')
+  plt.show()
